d6f7c6c779cf15ee59c04598fd672a3258e22f74
coloful <langsonwap@gmail.com>
2015-08-26 17:04:04 +0700

    Create them.php

---
+++ them.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+        <form method="post">
+            <p>ten:<input type="text" name="ten"></p>
+            <input type="submit" name="submit" value="them">
+        </form>
+    </body>
+</html>
